c7ddec6e6cf4619a87e213e305f6f4b62494dd4d
diksha16017 <diksha16017@iiitd.ac.in>
2017-08-06 16:20:10 +0530

    pls2

---
+++ udacity1/dictionary1.py
@@ -0,0 +1,6 @@
+# dictionary is key-value pair
+elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H'},
+            'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He'}}
+
+print (elements)
+print (" weight of hydrogen is : "+ str(elements['hydrogen']['weight']))
